5fc8a0962dbc15abbefa74052be26b7bfccb9826
Nathan Wycoff <nathanbrwycoff@gmail.com>
2018-06-25 18:59:52 -0500

    Some work on req corrs

---
+++ R/freq_corr_table.R
@@ -0,0 +1,58 @@
+#!/usr/bin/Rscript
+#  R/freq_corr_table.R Author "Nathan Wycoff <nathanbrwycoff@gmail.com>" Date 06.25.2018
+
+require(xtable)
+
+## Make the correlation tables.
+
+get_sig <- function(est, p) {
+    if (p < 0.001) {
+        return(paste(est, '^***', sep = ''))
+    } 
+    if (p < 0.1) {
+        return(paste(est, '^**', sep = ''))
+    }
+    if (p < 0.05) {
+        return(paste(est, '^*', sep = ''))
+    }
+}
+#Read in data
+datapath <- './RData'
+files <- list.files(datapath)
+outnames <- strsplit(files, '_')
+file_df <- data.frame(
+              name = sapply(outnames, function(x) paste(x[1:2], collapse = '_')),
+              semester = sapply(outnames, function(x) paste(x[3:4], collapse = '_')),
+              file = files)
+file_df$name <- as.character(file_df$name)
+
+pvals <- list()
+points <- list()
+fi <- 0
+for (f in file_df$file) {
+    fi <- fi + 1
+    load(paste(datapath, f, sep = '/'))
+    p <- ncol(X)
+    pval <- matrix(NA, nrow = p, ncol = p)
+    point <- matrix(NA, nrow = p, ncol = p)
+    for (i in 1:ncol(X)) {
+        for (j in 1:i) {
+            ret <- cor.test(X[,i], X[,j])
+            pval[i,j] <- pval[j,i] <- ret$p.val
+            point[i,j] <- point[j,i] <- ret$estimate
+        }
+    }
+    colnames(pval) <- rownames(pval) <- colnames(point) <- 
+        rownames(point) <- colnames(bayes_fit$mean)
+
+    # Make a table.
+    repr <- matrix("", nrow = nrow(point), ncol = ncol(pval))
+    for (i in 1:nrow(point)) {
+        for (j in 1:i) {
+            repr[i,j] <- repr[j,i] <- get_sig(point[i,j], pval[i,j])
+        }
+    }
+
+    print(xtable(repr), sanitize.text.function=identity, 
+          file = paste('latex_out/freq_tables/freq_', strsplit(f, '\\.')[[1]][1], '.tex', sep = ''))
+}
